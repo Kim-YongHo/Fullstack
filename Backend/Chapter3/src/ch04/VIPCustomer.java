47b0edfb544f33a8e5a0f6cebf4cb53b63ab1588
YongHo_Kim <ashore92@naver.com>
2022-05-06 17:57:44 +0900

    학습

---
+++ Backend/Chapter3/src/ch04/VIPCustomer.java
@@ -0,0 +1,33 @@
+package ch04;
+
+public class VIPCustomer extends Customer {
+	
+	double salesRatio;
+	private String agentID;
+	
+	
+	//super를 이용하여 상위 클래스의 생성자 명시적으로 호출
+	public VIPCustomer(int customerID, String customerName) {
+			super(customerID, customerName);
+			
+			customerGrade = "VIP";
+			bonusRatio = 0.05;
+			salesRatio = 0.1;
+	
+	}
+	
+	@Override
+	public int calPrice(int price) {
+		
+		bonusPoint += price * bonusRatio;
+		price -= (int)(price * salesRatio);
+		
+		return price;
+	}
+
+	public String getAgentID() {
+		return agentID;
+	}
+
+	
+}
